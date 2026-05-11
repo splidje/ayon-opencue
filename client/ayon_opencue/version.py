ae28f3fc55ead334fb311958a071ccbf528411b1
splidje <jonathan.hearn@ajptechnical.com>
2026-05-11 14:39:23 +0100

    tagged as v0.2.0

--- client/ayon_opencue/version.py
+++ client/ayon_opencue/version.py
@@ -1,2 +1,2 @@
 """Package declaring AYON addon 'opencue' version."""
-__version__ = "0.1.1+dev"
+__version__ = "0.2.0"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "opencue"
 title = "OpenCue"
-version = "0.1.1+dev"
+version = "0.2.0"
 client_dir = "ayon_opencue"
 ayon_required_addons = {}
 ayon_compatible_addons = {}
