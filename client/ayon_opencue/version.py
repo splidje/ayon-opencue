1f0a6c51cccf32c8b9a4bdec21582613a928bd7e
splidje <jonathan.hearn@ajptechnical.com>
2026-05-15 12:36:24 +0100

    upped version to 0.3.0

--- client/ayon_opencue/version.py
+++ client/ayon_opencue/version.py
@@ -1,2 +1,2 @@
 """Package declaring AYON addon 'opencue' version."""
-__version__ = "0.2.1"
+__version__ = "0.3.0"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "opencue"
 title = "OpenCue"
-version = "0.2.1"
+version = "0.3.0"
 client_dir = "ayon_opencue"
 ayon_required_addons = {}
 ayon_compatible_addons = {}
